3f6c447020aa4527d5ca71391c04e618f3b35329
ksivvi0 <ksivvi0@gmail.com>
2021-08-15 21:07:31 +0500

    add GetTokenPair method in auth service

--- internal/services/auth.go
+++ internal/services/auth.go
@@ -1,52 +1,105 @@
 package services
 
 import (
+	"errors"
 	"github.com/dgrijalva/jwt-go"
 	"github.com/google/uuid"
 	"time"
 )
 
 type Auth struct {
-	accessTokenTTL  time.Duration
-	accessTokenKey  string
+	accessTokenInfo
+	refreshTokenInfo
+}
+
+type accessTokenInfo struct {
+	accessTokenTTL time.Duration
+	accessTokenKey string
+	token          string
+}
+
+type refreshTokenInfo struct {
 	refreshTokenTTL time.Duration
 	refreshTokenKey string
+	token           string
 }
 
 func NewAuth() *Auth {
 	return &Auth{
-		accessTokenTTL:  time.Minute * 5,
-		accessTokenKey:  "99873dachas7d",
-		refreshTokenTTL: time.Hour * 8,
-		refreshTokenKey: "aklsjdfhsdfh1",
+		accessTokenInfo: accessTokenInfo{
+			accessTokenTTL: time.Minute * 5,
+			accessTokenKey: "99873dachas7d", //insecure
+		},
+		refreshTokenInfo: refreshTokenInfo{
+			refreshTokenTTL: time.Hour * 24,
+			refreshTokenKey: "aklsjdfhsdfh1", //insecure
+		},
 	}
+}
+
+type tokenPair struct {
+	accessToken  string
+	refreshToken string
 }
 
 type tokenClaims struct {
 	jwt.StandardClaims
-	GUID uuid.UUID
+	uuid uuid.UUID
 }
 
-//func (a *Auth) GenerateToken(username, password string) (string, error) {
-//svc := NewNebulaService(a.nebulaAddress)
-//
-//if !svc.CheckAuthorization(username, password) {
-//	return "", errors.New("неверные аутентификационные данные")
-//}
-//
-//token := jwt.NewWithClaims(
-//	jwt.SigningMethodHS256,
-//	&tokenClaims{
-//		jwt.StandardClaims{
-//			ExpiresAt: time.Now().Add(a.tokenTTL).Unix(),
-//			IssuedAt:  time.Now().Unix(),
-//		},
-//		username,
-//		helpers.EncodeThis(password),
-//	},
-//)
-//return token.SignedString([]byte(a.tokenKey))
-//}
+func (a *Auth) GetTokenPair() (*tokenPair, error) {
+	newUuid, err := uuid.NewUUID()
+	if err != nil {
+		return nil, err
+	}
+	accessToken, err := a.generateToken(newUuid, a.accessTokenInfo)
+	refreshToken, err := a.generateToken(newUuid, a.refreshTokenInfo)
+
+	pair := &tokenPair{
+		accessToken:  accessToken,
+		refreshToken: refreshToken,
+	}
+	return pair, nil
+}
+
+func (a *Auth) generateToken(uuid uuid.UUID, tokenInfo interface{}) (string, error) {
+	switch tokenInfo.(type) {
+	case accessTokenInfo:
+		claims := jwt.NewWithClaims(
+			jwt.SigningMethodHS512,
+			tokenClaims{
+				StandardClaims: jwt.StandardClaims{
+					ExpiresAt: time.Now().Add(a.accessTokenTTL).Unix(),
+					IssuedAt:  time.Now().Unix(),
+				},
+				uuid: uuid,
+			},
+		)
+		aToken, err := claims.SignedString(a.accessTokenKey)
+		if err != nil {
+			return "", err
+		}
+		return aToken, nil
+	case refreshTokenInfo:
+		claims := jwt.NewWithClaims(
+			jwt.SigningMethodHS512,
+			tokenClaims{
+				StandardClaims: jwt.StandardClaims{
+					ExpiresAt: time.Now().Add(a.accessTokenTTL).Unix(),
+					IssuedAt:  time.Now().Unix(),
+				},
+				uuid: uuid,
+			},
+		)
+		rToken, err := claims.SignedString(a.accessTokenKey)
+		if err != nil {
+			return "", err
+		}
+		return rToken, nil
+	default:
+		return "", errors.New("invalid argument")
+	}
+}
 
 //func (a *Auth) ParseToken(tokenIn string) ([]string, error) {
 //token, err := jwt.ParseWithClaims(tokenIn, &tokenClaims{}, func(token *jwt.Token) (interface{}, error) {
