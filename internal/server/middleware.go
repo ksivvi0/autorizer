402e6cda9363ea0f0e16f90c37e7cde2d31267e8
ksivvi0 <ksivvi0@gmail.com>
2021-08-16 13:08:36 +0500

    Update middleware.go

--- internal/server/middleware.go
+++ internal/server/middleware.go
@@ -1,9 +1,11 @@
 package server
 
 import (
+	"errors"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strings"
 	"time"
 )
 
@@ -25,10 +27,28 @@ func (s *Server) authMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		bearerHeader := c.Request.Header.Get("Authorization")
 		if len(bearerHeader) == 0 {
-			c.JSON(http.StatusForbidden, gin.H{"error": "empty authorization header"})
+			s.errorResponder(c, http.StatusUnauthorized, errors.New("empty authorization header"))
 			c.Abort()
 		}
-		//todo: add parsing auth token
+
+		bearerHeaderArr := strings.Split(bearerHeader, " ")
+		if len(bearerHeaderArr) != 2 || bearerHeaderArr[0] != "Bearer" {
+			s.errorResponder(c, http.StatusUnauthorized, errors.New("invalid authorization header"))
+			c.Abort()
+		}
+
+		token := bearerHeaderArr[1]
+		if token == "" {
+			s.errorResponder(c, http.StatusUnauthorized, errors.New("empty authorization token"))
+			c.Abort()
+		}
+
+		_uuid, err := s.services.AuthService.ParseToken(token)
+		if err != nil {
+			s.errorResponder(c, http.StatusForbidden, err)
+			c.Abort()
+		}
+		c.Set("uuid", _uuid)
 		c.Next()
 	}
 }
