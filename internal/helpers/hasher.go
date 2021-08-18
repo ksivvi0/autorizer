7f6de90344e8e7429b07c4b445e5c434f13a9336
ksivvi0 <ksivvi0@gmail.com>
2021-08-18 21:06:02 +0500

    big deal

--- internal/helpers/hasher.go
+++ internal/helpers/hasher.go
@@ -1,6 +1,9 @@
 package helpers
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	"encoding/base64"
+	"golang.org/x/crypto/bcrypt"
+)
 
 func GetHash(data string) (string, error) {
 	raw, err := bcrypt.GenerateFromPassword([]byte(data), 11)
@@ -13,4 +16,16 @@ func GetHash(data string) (string, error) {
 func CheckHash(want string, hash string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(want))
 	return err == nil
+}
+
+func EncodeString(data string) string {
+	return base64.StdEncoding.EncodeToString([]byte(data))
+}
+
+func DecodeString(data string) (string, error) {
+	rawStr, err := base64.StdEncoding.DecodeString(data)
+	if err != nil {
+		return "", err
+	}
+	return string(rawStr), nil
 }
---
+++ internal/helpers/hasher_test.go
@@ -0,0 +1,46 @@
+package helpers
+
+import "testing"
+
+func TestGetHash(t *testing.T) {
+	cases := []string{"hello", "привет, мир!"}
+	for _, v := range cases {
+		result, err := GetHash(v)
+		if err != nil {
+			t.Errorf("Фраза '%s' выдала ошибку %s", v, err.Error())
+		}
+		if !CheckHash(v, result) {
+			t.Errorf("Несовпадение: '%s', '%s'", v, result)
+		}
+	}
+}
+
+func TestEncodeString(t *testing.T) {
+	cases := map[string]string{
+		"hello": "aGVsbG8=",
+		"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjEzMzciLCJ1c2VybmFtZSI6ImJpem9uZSIsImlhdCI6MTU5NDIwOTYwMCwicm9sZSI6InVzZXIifQ.ZvkYYnyM929FM4NW9_hSis7_x3_9rymsDAx9yuOcc1I": "ZXlKMGVYQWlPaUpLVjFRaUxDSmhiR2NpT2lKSVV6STFOaUo5LmV5SnBaQ0k2SWpFek16Y2lMQ0oxYzJWeWJtRnRaU0k2SW1KcGVtOXVaU0lzSW1saGRDSTZNVFU1TkRJd09UWXdNQ3dpY205c1pTSTZJblZ6WlhJaWZRLlp2a1lZbnlNOTI5Rk00Tlc5X2hTaXM3X3gzXzlyeW1zREF4OXl1T2NjMUk=",
+	}
+	for k, v := range cases {
+		r := EncodeString(k)
+		if r != v {
+			t.Errorf("%s: получено '%s', ожидалось '%s'", k, r, v)
+		}
+	}
+}
+
+func TestDecodeString(t *testing.T) {
+	cases := map[string]string{
+		"aGVsbG8=":                     "hello",
+		"0L/RgNC40LLQtdGCLCDQvNC40YAh": "привет, мир!",
+		"ZXlKMGVYQWlPaUpLVjFRaUxDSmhiR2NpT2lKSVV6STFOaUo5LmV5SnBaQ0k2SWpFek16Y2lMQ0oxYzJWeWJtRnRaU0k2SW1KcGVtOXVaU0lzSW1saGRDSTZNVFU1TkRJd09UWXdNQ3dpY205c1pTSTZJblZ6WlhJaWZRLlp2a1lZbnlNOTI5Rk00Tlc5X2hTaXM3X3gzXzlyeW1zREF4OXl1T2NjMUk=": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjEzMzciLCJ1c2VybmFtZSI6ImJpem9uZSIsImlhdCI6MTU5NDIwOTYwMCwicm9sZSI6InVzZXIifQ.ZvkYYnyM929FM4NW9_hSis7_x3_9rymsDAx9yuOcc1I",
+	}
+	for k, v := range cases {
+		r, err := DecodeString(k)
+		if err != nil {
+			t.Errorf("%s: %v", k, err)
+		}
+		if r != v {
+			t.Errorf("%s: получено '%s', ожидалось '%s'", k, r, v)
+		}
+	}
+}
